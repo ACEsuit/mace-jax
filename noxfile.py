77235c4df801d7036bef24a364d659aa05211169
Mario Geiger <geiger.mario@gmail.com>
2022-12-14 14:03:59 -0500

    fix nox

--- noxfile.py
+++ noxfile.py
@@ -4,5 +4,6 @@ import nox
 @nox.session
 def tests(session):
     session.install("pip")
+    session.run("pip", "install", "numpy")  # required to install matscipy
     session.run("python", "setup.py", "develop")
     session.run("python", "-m", "mace_jax.run_train", "configs/aspirin_small.gin")
